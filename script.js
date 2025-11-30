d24812539bca7964d0a8f52425154e126331bd73
OxiMika <charliemorningstar0785@gmail.com>
2025-11-30 23:09:52 +0330

    new

---
+++ script.js
@@ -0,0 +1,19 @@
+<script type="module">
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAyGhDkqAwyCv-Sqa8z4BbkNa_SrpXv4Zk",
+    authDomain: "mika-b7f7c.firebaseapp.com",
+    projectId: "mika-b7f7c",
+    storageBucket: "mika-b7f7c.firebasestorage.app",
+    messagingSenderId: "524357269646",
+    appId: "1:524357269646:web:89548b32616ebcbe4a31df"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+</script>
